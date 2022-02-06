4e04c8973ea0a80ca163d1f763a9f8270c6ce3c6
Anatoly <darcstile@mail.ru>
2022-02-06 18:06:21 +0700

    create test v 1

--- src/test/java/com/gh/mirocodij/inventory/system/springinventorysystem/integration/DemoControllerTest.java
+++ src/test/java/com/gh/mirocodij/inventory/system/springinventorysystem/integration/DemoControllerTest.java
@@ -1,28 +1,28 @@
 package com.gh.mirocodij.inventory.system.springinventorysystem.integration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gh.mirocodij.inventory.system.springinventorysystem.fixtures.BaseIntegrationTest;
 import com.gh.mirocodij.inventory.system.springinventorysystem.model.DemoModelDto;
-import com.gh.mirocodij.inventory.system.springinventorysystem.repository.DemoModelRepository;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 public class DemoControllerTest extends BaseIntegrationTest {
-
-    @Autowired
-    public DemoModelRepository demoModelRepository;
 
     @Autowired
     public MockMvc mockMvc;
@@ -34,8 +34,17 @@ public class DemoControllerTest extends BaseIntegrationTest {
         super("/demo");
     }
 
-    @RepeatedTest(3)
-    public void getMethodShouldReturnAllTheDemoModels() throws Exception {
+    @BeforeEach
+    public void beforeEach() throws Exception {
+        for (int i = 0; i < 10; i++) {
+            DemoModelDto demoModelDto = new DemoModelDto();
+            demoModelDto.setDemoField("field" + i);
+            sendPost(demoModelDto);
+        }
+    }
+
+    @Test
+    public void postMethodShouldSavedModelAndReturnIt() throws Exception {
         DemoModelDto demoModelDto = new DemoModelDto();
         demoModelDto.setDemoField("another value");
         MockHttpServletResponse response = mockMvc.perform(post(baseUrl)
@@ -49,5 +58,75 @@ public class DemoControllerTest extends BaseIntegrationTest {
         assertThat(demoModelFromResponse).isNotNull();
         assertThat(demoModelFromResponse.getDemoField()).isEqualTo("another value");
         assertThat(demoModelFromResponse.getId()).isNotNull();
+    }
+
+    @Test
+    public void postMethodShouldHaveBadRequestWhenTrySavedModel() throws Exception {
+        DemoModelDto savedDtoModel = new DemoModelDto();
+        savedDtoModel.setDemoField("value which will repeat");
+        sendPost(savedDtoModel);
+        DemoModelDto notSavedDtoModel = new DemoModelDto();
+        notSavedDtoModel.setDemoField("value which will repeat");
+        var response = mockMvc.perform(post(baseUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(notSavedDtoModel)))
+                .andExpect(status().isBadRequest())
+                .andReturn().getResponse();
+    }
+
+    @Test
+    public void getMethodShouldReturnAllModels() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get(baseUrl))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(10))
+                .andReturn().getResponse();
+        var demoModelsFromResponse = mapper.readValue(response.getContentAsString(),
+                new TypeReference<List<DemoModelDto>>() {});
+        for (int i = 0; i < 10; i++) {
+            var demoModel = demoModelsFromResponse.get(i);
+            assertThat(demoModel).isNotNull();
+            assertThat(demoModel.getDemoField()).isEqualTo("field" + i);
+            assertThat(demoModel.getId()).isNotNull();
+        }
+    }
+
+    @Test
+    public void getMethodShouldReturnModelById() throws Exception {
+        DemoModelDto demoModelDto = new DemoModelDto();
+        demoModelDto.setDemoField("test field by id");
+        var postResponse = mockMvc.perform(post(baseUrl)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(demoModelDto)))
+                .andReturn().getResponse();
+        var demoModelFromPostResponse = mapper.readValue(postResponse.getContentAsString(), DemoModelDto.class);
+        var id = demoModelFromPostResponse.getId();
+        var getResponse = mockMvc.perform(get(baseUrl + "/" + id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andReturn().getResponse();
+        var demoModelFromGetResponse = mapper.readValue(getResponse.getContentAsString(), DemoModelDto.class);
+        assertThat(id).isNotNull();
+        assertThat(demoModelFromPostResponse.getDemoField()).isNotNull();
+        assertThat(demoModelFromGetResponse.getDemoField()).isNotNull();
+        assertThat(demoModelFromGetResponse).isNotNull();
+        assertThat(demoModelFromGetResponse.getId()).isNotNull();
+        assertThat(demoModelFromGetResponse.getId()).isEqualTo(id);
+        assertThat(demoModelFromGetResponse.getDemoField()).isEqualTo(demoModelFromPostResponse.getDemoField());
+    }
+
+    @Test
+    public void getMethodShouldHaveNotFoundStatusIfIdNotExist() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        mockMvc.perform(get(baseUrl + "/" + uuid))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+
+
+    private void sendPost(DemoModelDto demoModelDto) throws Exception {
+        mockMvc.perform(post(baseUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(demoModelDto)));
     }
 }
